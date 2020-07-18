c0bd86347a38975ddd84dbc00919e68d29f89f96
James <jamesgrubb@mac.com>
2020-07-18 12:30:12 +0100

    removed function

--- functions/screenGrab.js
+++ functions/screenGrab.js
@@ -13,20 +13,18 @@ exports.handler = async (event, ctx) => {
         const context = await browser._defaultContext;
         const page = await context.newPage();
         await page.goto(url || 'https://www.jamesgrubb.co.uk');
-        const screenshotPage = async (page) => {
+        const buffer = await page.screenshot()
+        const imageBuffer = await buffer.toString('base64')
+        const uploadedResponse = await cloudinary.uploader.upload(imageBuffer, {
+            upload_preset: 'dev_upload'
+        })
+        console.log("screenshotPage -> uploadedResponse", uploadedResponse)
 
-            let buffer = await page.screenshot()
-            let imageBuffer = await buffer.toString('base64')
-            let uploadedResponse = await cloudinary.uploader.upload(imageBuffer, {
-                upload_preset: 'dev_upload'
-            })
-            console.log("screenshotPage -> uploadedResponse", uploadedResponse)
 
-        }
-        await screenshotPage(page)
+
         // const grab = await page.screenshot({ encoding: "base64" })
 
-        // console.log("exports.handler -> uploadedResponse", uploadedResponse)
+        console.log("exports.handler -> uploadedResponse", uploadedResponse)
 
         console.log('Page title: ', await page.screenshot({ encoding: "base64" }));
         // console.log('Page title: ', await page.title());
